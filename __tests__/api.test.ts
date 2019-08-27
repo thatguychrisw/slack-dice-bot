f5ccb88aeccee4f861a7f23549415ebe7ca976e6
Christopher Willard <thatguychrisw@gmail.com>
2019-08-26 23:00:53 -0400

    Add 🎲 rolls

--- __tests__/api.test.ts
+++ __tests__/api.test.ts
@@ -4,3 +4,7 @@ import app from '../app'
 test('it responds successfully to GET /', () => {
     return request(app).get('/').expect(200);
 });
+
+test('it responds successfully on a POST /', () => {
+    return request(app).post('/').expect(200);
+});
--- app.ts
+++ app.ts
@@ -5,5 +5,14 @@ const app = express();
 
 app.get('/', (request, response) => response.send('💙 Netlify Functions!'));
 
+app.post('/', (request, response) => {
+    const rollDice = (max: number):number => Math.floor(Math.random() * (max - 1)) + 1;
+
+    let result = rollDice(6).toString();
+
+    response.send(`You rolled a ${result} on a six sided dice.`);
+});
+
+
 export default app;
 export const handler = serverless(app);
